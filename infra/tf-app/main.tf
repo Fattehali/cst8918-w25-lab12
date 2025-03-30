0d0bb11ef0b2825da00ef5e3accf78dfbdaeec0a
sfattehali72@gmail.com <sfattehali72@gmail.com>
2025-03-30 00:33:26 -0400

    Create the base Terraform configuration for the app infrastructure

---
+++ infra/tf-app/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "app_rg" {
+  name     = "suna0011-shar0055-a12-rg"
+  location = "East US"
+}
---
+++ infra/tf-app/terraform.tf
@@ -0,0 +1,37 @@
+terraform {
+
+  required_providers {
+
+    azurerm = {
+
+      source = "hashicorp/azurerm"
+
+      version = "~> 3.0"
+
+    }
+
+  }
+
+  backend "azurerm" {
+
+    resource_group_name = "suna0011-shar0055-githubactions-rg"
+
+    storage_account_name = "suna0011shar0055"
+
+    container_name = "tfstate"
+
+    key = "prod.app.tfstate"
+
+  }
+
+  required_version = ">= 1.3.0"
+
+}
+
+provider "azurerm" {
+
+  features {}
+
+}
+
+ 
