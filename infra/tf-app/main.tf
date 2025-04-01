7a4e1ddcc91a3d9a0ea8b46e26b2e31403c2a11e
sfattehali72@gmail.com <sfattehali72@gmail.com>
2025-04-01 00:08:52 -0400

    Added Virtual Network and Subnet

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -5,14 +5,15 @@ resource "azurerm_resource_group" "app_rg" {
 
 resource "azurerm_virtual_network" "main_vnet" {
   name                = "lab12-vnet"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.app_rg.location # Corrected
+  resource_group_name = azurerm_resource_group.app_rg.name     # Corrected
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "main_subnet" {
   name                 = "lab12-subnet"
-  resource_group_name  = azurerm_resource_group.main.name
+  resource_group_name  = azurerm_resource_group.app_rg.name # Corrected
   virtual_network_name = azurerm_virtual_network.main_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
